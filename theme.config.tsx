28cb10f4773b10d2fa8c10345914fd15601021de
sciforearth <sciforearth@gmail.com>
2025-01-06 11:43:05 +0530

    changes in folder structure

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Nodemongo documentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Sciforearth/nodemongo-blog',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Sciforearth/nodemongo-blog',
   footer: {
     text: 'Nodemongo Documentation',
   },
