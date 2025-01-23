a7f3725d404b08c9bc2548f44b0025dc9694d549
sciforearth <sciforearth@gmail.com>
2025-01-23 11:33:19 +0530

    revert

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-import nextra from 'nextra';
-
-const withNextra = nextra({
+const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-});
+})
 
-export default withNextra();
+module.exports = withNextra()
